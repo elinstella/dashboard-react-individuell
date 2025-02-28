f3dd550d086fe258821c6bf177be87ef0b536887
gragas <180623g3h34htctrl@users.noreply.github.com>
2025-02-28 09:18:03 +0100

    upodate

--- dashboard-lkpg/src/App.jsx
+++ dashboard-lkpg/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function App() {
   return (
     <>
-      <h1 className='text-red-500 text-xl'>Hello world</h1>
+      <h1 className='text-red-500 text-xl bg-amber-800'>Hello world</h1>
     </>
   );
 }

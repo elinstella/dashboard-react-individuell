b9cc7864bfd0815ff0cb7ffe1f6cbd32169f779b
gragas <180623g3h34htctrl@users.noreply.github.com>
2025-02-28 10:05:22 +0100

    mock data

--- dashboard-lkpg/src/App.jsx
+++ dashboard-lkpg/src/App.jsx
@@ -5,7 +5,16 @@ function App() {
   return (
     <>
       <h1 className='text-red-500 text-xl bg-amber-800'>Hello world</h1>
+
       {console.log(mockData)}
+
+      {mockData.users.map((user, index) => (
+        <div>
+          <h2>{user.name}</h2>
+          <h3>{user.location}</h3>
+          <h3>{user.email}</h3>
+        </div>
+      ))}
     </>
   );
 }

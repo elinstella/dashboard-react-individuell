3a797f31311b3283711667414607af0820b512df
gragas <180623g3h34htctrl@users.noreply.github.com>
2025-02-28 09:48:21 +0100

    add mockdata

--- dashboard-lkpg/src/App.jsx
+++ dashboard-lkpg/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import mockData from './data/mockData.json';
 
 function App() {
   return (
     <>
       <h1 className='text-red-500 text-xl bg-amber-800'>Hello world</h1>
+      {console.log(mockData)}
     </>
   );
 }

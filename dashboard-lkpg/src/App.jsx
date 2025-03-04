a52a6d1fc031087190cb17d09384eb5496586bcc
Sekaziga <145746970+Sekaziga@users.noreply.github.com>
2025-03-04 13:38:20 +0100

    gaston/component

--- dashboard-lkpg/src/App.jsx
+++ dashboard-lkpg/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react';
 import Dashboard from './components/Dashboard';
 import NavHeader from './components/NavHeader';
 import Footer from './components/Footer';
+import UserLeaderboard from './components/UserLeaderboard';
+import mockData from './data/mockData.json';
 
 function App() {
   return (
     <>
+    <UserLeaderboard usersData={mockData} />
       <NavHeader />
       <Dashboard />
-      <Footer />
+      <Footer /> 
     </>
   );
 }

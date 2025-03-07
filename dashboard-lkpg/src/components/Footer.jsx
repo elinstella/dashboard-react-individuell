fc7594505ed607e056ebe3ff32718ff07f3bd418
elinstella <elin_elsa@hotmail.com>
2025-03-07 11:21:49 +0100

    Tillägg av primära och sekundära färger samt css

--- dashboard-lkpg/src/components/Footer.jsx
+++ dashboard-lkpg/src/components/Footer.jsx
@@ -1,7 +1,26 @@
+import React from 'react';
+
 function Footer() {
   return (
-    <footer className='bg-blue-300 p-3 mt-8'>
-      <p>Copyright 2025 BeatStats</p>
+    <footer className="bg-gradient-custom text-white py-6 border-t border-quaternary">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+        <p className="text-sm text-center">
+          Copyright 2025 BeatStats
+        </p>
+
+        {/* Social media links or other footer content */}
+        <div className="flex space-x-4">
+          <a href="#" className="text-white hover:text-tertiary transition-all duration-300">
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a href="#" className="text-white hover:text-tertiary transition-all duration-300">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#" className="text-white hover:text-tertiary transition-all duration-300">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
